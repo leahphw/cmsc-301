2d349602309e788c2cc7dca7ca2177353e42406f
leahphw <leah.phw@gmail.com>
2024-02-23 14:23:37 -0500

    work

--- aa03/game.asm
+++ aa03/game.asm
@@ -1,9 +1,31 @@
           .data 
+          PlayerVMT: .word playerAttack playerTakeDamage playerSellItem playerPickUpItem
+          MerchantVMT: .word playerAttack playerTakeDamage merchantSellItem merchantPickUpItem
+          WarriorVMT: .word warriorAttack playerTakeDamage playerSellItem playerPickUpItem
+          KnightVMT: .word warriorAttack knightTakeDamage playerSellItem playerPickUpItem
 
           .text
           .align 2 #This is boilerplate stuff to get QTSPIM to read this file the right way
           .globl main 
 # Implement your classes here.
+itemConstructor:                            # Takes 4 args: address, gv, ab, armb
+    lw $a1, 4($a0)                          # goldValue = gv
+    lw $a2, 8($a0)                          # attackBonus = ab
+    lw $a3, 12($a0)                         # armorBonus = armb
+    jr $ra                                  # return
+
+playerConstructor:                          # Takes 4 args: address, mh, g, s
+    lw $a1, 4($a0)                          # max_hp = mh
+    lw $a2, 8($a0)                          # currect_hp = mh
+    lw $a3, 12($a0)                         # 
+
+playerAttack: 
+
+playerTakeDamage: 
+
+playerSellItem:
+
+playerPickUpItem:
 
 testPlayer:
     # Implement your testPlayer function here.
